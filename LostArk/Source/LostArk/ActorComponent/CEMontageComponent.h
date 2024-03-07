ad26011ead8cbf5bcbab78da4da525ece392d9eb
Arkchimy <ajffkeneod12@naver.com>
2024-03-07 21:20:12 +0900

    커밋되돌리기 2

---
+++ LostArk/Source/LostArk/ActorComponent/CEMontageComponent.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CMontageComponent.h"
+#include "CEMontageComponent.generated.h"
+
+UCLASS()
+class LOSTARK_API UCEMontageComponent : public UCMontageComponent
+{
+    GENERATED_BODY()
+  public:
+    UCEMontageComponent() = default;
+
+  protected:
+    virtual void BeginPlay() override;
+
+};
+
+
