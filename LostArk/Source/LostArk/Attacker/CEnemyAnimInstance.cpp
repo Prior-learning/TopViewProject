b8079dd015b4f6a237a0a4f7165f01d90de952cc
Arkchimy <ajffkeneod12@naver.com>
2024-03-12 15:58:46 +0900

    Animinstance 와 StateComponent 구조 개선

--- LostArk/Source/LostArk/Attacker/CEnemyAnimInstance.cpp
+++ LostArk/Source/LostArk/Attacker/CEnemyAnimInstance.cpp
@@ -1,26 +1,14 @@
 #include "CEnemyAnimInstance.h"
-#include "../Global.h"
 #include "GameFramework/Character.h"
-#include "../ActorComponent/CEnemyStateComponent.h"
+#include "../ActorComponent/CStateComponent.h"
 
 void UCEnemyAnimInstance::NativeBeginPlay()
 {
 	Super::NativeBeginPlay();
-
-	OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
 }
 
 void UCEnemyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
-
-	CheckNull(OwnerCharacter);
 	Super::NativeUpdateAnimation(DeltaSeconds);
 
-	Speed = OwnerCharacter->GetVelocity().Size2D();
-	Direction = CalculateDirection(OwnerCharacter->GetVelocity(), OwnerCharacter->GetControlRotation());
-	Pitch = OwnerCharacter->GetBaseAimRotation().Pitch;
-
-	UCEnemyStateComponent* state = CHelpers::GetComponent<UCEnemyStateComponent>(OwnerCharacter);
-    if (!!state)
-		bDeath = state->IsDeathMode();
 }
