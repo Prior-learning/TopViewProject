3269f019caf4bed69a753b37be20353617d42f76
Arkchimy <ajffkeneod12@naver.com>
2024-03-13 18:24:35 +0900

    EState Flag 로 구현하기 완료 ,  에러 현재까진 못봄

--- LostArk/Source/LostArk/BehaviorTreeService/CBTTask_Arrive.cpp
+++ LostArk/Source/LostArk/BehaviorTreeService/CBTTask_Arrive.cpp
@@ -1,13 +1,16 @@
 #include "CBTTask_Arrive.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include <cmath>
-#include "../Utilities/CLog.h"
+#include "../Global.h"
 #include "AIController.h"
+#include "../ActorComponent/CEnemyStateComponent.h"
 
 EBTNodeResult::Type UCBTTask_Arrive::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8 *NodeMemory)
 {
     FVector movepos = OwnerComp.GetBlackboardComponent()->GetValueAsVector("MovePos");
     AActor* self = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("Self"));
+    UCEnemyStateComponent* statecomp = CHelpers::GetComponent<UCEnemyStateComponent>(self);
+
     if (!!self)
     {
         FVector current = self->GetActorLocation();
@@ -15,7 +18,8 @@ EBTNodeResult::Type UCBTTask_Arrive::ExecuteTask(UBehaviorTreeComponent &OwnerCo
         float distance = sqrt(pow(movepos.X - current.X, 2) + pow(movepos.Y - current.Y, 2));
         if (distance <= mNearDistance)
         {
-            OwnerComp.GetBlackboardComponent()->SetValueAsEnum("State", 0);
+            statecomp->Remove(E_WHY_BLOCKED::MOVE);
+            statecomp->SetMode(EStateEnemyType::Idle, E_WHY_BLOCKED::NONE);
             return EBTNodeResult::Succeeded;
         }
         return EBTNodeResult::Failed;
