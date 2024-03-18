69e0ef9b3eeef810638f63f19d9477bb17895890
LAPTOP-FHR9RH5N\D <kdoyun756@gmail.com>
2024-03-18 14:29:30 +0900

    플러그인 뺴놓기

--- LostArk/Source/LostArk/ObjectPools/CObjectPoolTest.cpp
+++ LostArk/Source/LostArk/ObjectPools/CObjectPoolTest.cpp
@@ -1,5 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+#include "CObjectPoolTest.h"
 
+UCObjectPoolTest::UCObjectPoolTest()
+{
+}
 
-#include "ObjectPools/CObjectPoolTest.h"
 
--- LostArk/Source/LostArk/ObjectPools/CObjectPoolTest.h
+++ LostArk/Source/LostArk/ObjectPools/CObjectPoolTest.h
@@ -1,17 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "CObjectPoolTest.generated.h"
 
-/**
- * 
- */
+//template <typename T>
+
 UCLASS()
 class LOSTARK_API UCObjectPoolTest : public UObject
 {
 	GENERATED_BODY()
+
+public:
+    UCObjectPoolTest();
+  //static T *Pop(){};
+
 	
 };
