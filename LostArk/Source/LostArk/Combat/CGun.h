8aaccfe849a810871df3a42ce6bd3390804b0464
LAPTOP-FHR9RH5N\D <kdoyun756@gmail.com>
2024-03-21 17:03:14 +0900

    BulletManager 구현중... 터지지않게만 해두고 이어서 하겠습니다.

--- LostArk/Source/LostArk/Combat/CGun.h
+++ LostArk/Source/LostArk/Combat/CGun.h
@@ -14,7 +14,7 @@ class LOSTARK_API ACGun : public ACWeapon
 
   protected:
     UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
-    TSubclassOf<class ACBullet> mBullet;
+    TSubclassOf<class ACBulletManager> mBulletPool;
 
     UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
     FName mRifleHand;
---
+++ LostArk/Source/LostArk/ObjectPools/CBulletManager.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CBulletManager.generated.h"
+
+UCLASS()
+class LOSTARK_API ACBulletManager : public AActor
+{
+    GENERATED_BODY()
+
+  public:
+    ACBulletManager();
+
+    void Init();
+    void Expand();
+    void Return(class ACBullet *target);
+    class ACBullet *Pop();
+    static ACBulletManager &GetInstance();
+
+  protected:
+    virtual void BeginPlay() override;
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+    TSubclassOf<class ACBullet> bulletClass;
+
+  
+
+  public:
+
+  private:
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+    int32 mBulletPoolSize=30;
+    UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, meta = (AllowPrivateAccess = true))
+    int32 mExpandSize=10;
+    TQueue<ACBullet *> BulletPool;
+    static ACBulletManager *mInstance;
+
+};
