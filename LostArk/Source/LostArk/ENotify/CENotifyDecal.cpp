dfc133521c7c14dc9b6edbb9c58ed6f4ca1b9582
LAPTOP-FHR9RH5N\D <kdoyun756@gmail.com>
2024-04-10 16:21:25 +0900

    Grux 에셋추가

--- LostArk/Source/LostArk/ENotify/CENotifyDecal.cpp
+++ LostArk/Source/LostArk/ENotify/CENotifyDecal.cpp
@@ -16,7 +16,7 @@ void UCENotifyDecal::Notify(USkeletalMeshComponent *MeshComp, UAnimSequenceBase 
 
     FVector forward = MeshComp->GetOwner()->GetActorForwardVector().GetSafeNormal2D();
     FVector location = MeshComp->GetOwner()->GetActorLocation() + forward * mOffset.X; 
-    location.Z = -150.f;
+    location.Z = -250.f;
     FRotator rot = MeshComp->GetOwner()->GetActorRotation();
     
     switch (mDirection)
